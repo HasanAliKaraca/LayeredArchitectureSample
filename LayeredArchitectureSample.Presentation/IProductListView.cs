f3b1297c5a682474507be82cfefafaa1e0cad9e6
Hasan Ali Karaca <hasan.ali.karaca@hotmail.com>
2013-05-09 12:04:02 +0300

    completed Presentation Layer

---
+++ LayeredArchitectureSample.Presentation/IProductListView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayeredArchitectureSample.Service;
+
+namespace LayeredArchitectureSample.Presentation
+{
+    public interface IProductListView
+    {
+        Model.EnmCustomerType EnmCustomerType { get; }
+        string ErrorMessage { set; }
+
+        void Display(IList<ProductViewModel> Products);
+    }
+}
---
+++ LayeredArchitectureSample.Presentation/ProductListPresenter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayeredArchitectureSample.Service;
+
+namespace LayeredArchitectureSample.Presentation
+{
+    public class ProductListPresenter
+    {
+        private IProductListView _productListView;
+        private Service.ProductService _productService;
+
+        public ProductListPresenter(IProductListView ProductListView, Service.ProductService ProductService)
+        {
+            _productListView = ProductListView;
+            _productService = ProductService;
+        }
+
+        public void Display()
+        {
+            ProductListRequest productListRequest = new ProductListRequest();
+            productListRequest.EnmCustomerType = _productListView.EnmCustomerType;
+
+            ProductListResponse productResponse = _productService.GetAllProductsFor(productListRequest);
+            if (productResponse.Success == true)
+            {
+                _productListView.Display(productResponse.Products);
+            }
+            else
+            {
+                _productListView.ErrorMessage = productResponse.Message;
+            }
+        }
+    }
+}
