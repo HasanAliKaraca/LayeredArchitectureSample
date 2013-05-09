760303fb1329e8d7a4cf41642d87d1d5fe072d13
Hasan Ali Karaca <hasan.ali.karaca@hotmail.com>
2013-05-09 11:50:38 +0300

    added DAL

---
+++ LayeredArchitectureSample.Repository/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayeredArchitectureSample.Model;
+
+namespace LayeredArchitectureSample.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        #region IProductRepository Members
+
+        public IList<Model.Product> FindAll()
+        {
+            using (ShopDataContext context = new ShopDataContext())
+            {
+                var products = from p in context.Products
+                               select new Model.Product()
+                               {
+                                   Id = p.ProductId,
+                                   Name = p.ProductName,
+                                   Price = new Model.Price(p.RRP, p.SellingPrice)
+                               };
+
+                return products.ToList();
+            }
+        }
+
+        #endregion
+    }
+}
